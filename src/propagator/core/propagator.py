fix:fixed computation of output to consider nan values properly

src/propagator/core/propagator.py
@@ -128,7 +128,7 @@ class Propagator:
         numpy.ndarray
             2D array with max RoS per cell.
         """
-        RoS_max = np.max(self.ros, axis=2).astype(np.float32)
+        RoS_max = np.nanmax(self.ros, axis=2).astype(np.float32)
         return RoS_max
 
     def compute_ros_mean(self) -> npt.NDArray[np.floating]:
@@ -184,11 +184,11 @@ class Propagator:
         mask = self.fire > 0
 
         # accumulate in float64 to reduce precision loss
-        s = np.sum(np.where(mask, the_var, 0.0), axis=2, dtype=np.float64)
+        s = np.nansum(np.where(mask, the_var, 0.0), axis=2, dtype=np.float64)
         c = np.sum(mask, axis=2)
 
-        # mean where count>0; zero otherwise
-        out = np.full(self.veg.shape, 0.0, dtype=np.float32)
+        # mean where count>0; NaN otherwise
+        out = np.full(self.veg.shape, np.nan, dtype=np.float32)
         np.divide(s, c, out=out, where=c > 0)
         return out
 

src/propagator/io/geo.py
@@ -65,11 +65,12 @@ def trim_values(
     min_row, max_row = int(rows / 2 - 1), int(rows / 2 + 1)
     min_col, max_col = int(cols / 2 - 1), int(cols / 2 + 1)
 
-    v_rows = np.where(values.sum(axis=1) > 0)[0]
+    mask = values > 0
+    v_rows = np.where(mask.sum(axis=1) > 0)[0]
     if len(v_rows) > 0:
         min_row, max_row = v_rows[0] - 1, v_rows[-1] + 2
 
-    v_cols = np.where(values.sum(axis=0) > 0)[0]
+    v_cols = np.where(mask.sum(axis=0) > 0)[0]
     if len(v_cols) > 0:
         min_col, max_col = v_cols[0] - 1, v_cols[-1] + 2
 