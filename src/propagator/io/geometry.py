better cli and fix error in centroid calculation (#7)
* fix: fix typings and improve cli output

* fix: invert loader priority

* fix: fix get_middle_point function behaviour with polygons

* feat: improve cli output and add a flag for verbosity

src/propagator/io/geometry.py
@@ -6,11 +6,18 @@ from enum import Enum
 from typing import List, Optional, Sequence, Tuple, Union
 
 import numpy as np
-import rasterio.enums as rio_enums
+import rasterio.enums as rio_enums  # type: ignore
 import shapely
 from pyproj import CRS, Transformer
-from rasterio.features import rasterize
-from shapely import Geometry, LineString, Point, Polygon
+from rasterio.features import rasterize  # type: ignore
+from shapely import (
+    Geometry,
+    LineString,
+    MultiLineString,
+    MultiPolygon,
+    Point,
+    Polygon,
+)
 from shapely.ops import transform
 
 from propagator.io.geo import GeographicInfo
@@ -32,20 +39,30 @@ def get_middle_point(ignition: Geometry) -> Optional[Tuple[float, float]]:
     """
     if ignition is None:
         return None
-    coords = list(ignition.coords) if hasattr(ignition, "coords") else []  # type: ignore
 
-    # recursively collect coords if geometry has parts
-    if not coords and hasattr(ignition, "geoms"):
-        for g in ignition.geoms:  # type: ignore
-            sub = get_middle_point(g)
-            if sub:
-                coords.append(sub)
+    if isinstance(ignition, Point):
+        return (ignition.x, ignition.y)
 
-    if not coords:
+    elif isinstance(ignition, LineString):
+        coords = np.array(ignition.coords)
+        return tuple(coords.mean(axis=0))
+
+    elif isinstance(ignition, MultiLineString):
+        all_coords = np.concatenate(
+            [np.array(line.coords) for line in ignition.geoms]
+        )
+        return tuple(all_coords.mean(axis=0))
+
+    elif isinstance(ignition, Polygon):
+        return ignition.centroid.x, ignition.centroid.y
+
+    elif isinstance(ignition, MultiPolygon):
+        centroids = np.array(
+            [poly.centroid.coords[0] for poly in ignition.geoms]
+        )
+        return tuple(centroids.mean(axis=0))
+    else:
         return None
-
-    xs, ys = zip(*coords)
-    return (sum(xs) / len(xs), sum(ys) / len(ys))
 
 
 def reproject_geometry(